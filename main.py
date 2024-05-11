fe12b620d0b925b8b07aaf78ec71c5d3840cdb14
VIPTankz <t.y.l.e.r.14@live.co.uk>
2024-05-11 14:14:25 +0100

    setup main program

---
+++ main.py
@@ -0,0 +1,72 @@
+
+import numpy as np
+import time
+import torch
+import gymnasium as gym
+import os
+import argparse
+def make_env(game):
+
+    return gym.make("ALE/" + game + "-ram-v5")
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--game', type=str, default="BattleZone")
+
+    args = parser.parse_args()
+
+    game = args.game
+    print("Playing Game: " + str(game))
+
+    env = make_env(game)
+
+    agent_name = "Agent"
+    agent = None
+
+    n_steps = 5000000
+    steps = 0
+    done = False
+    observation, info = env.reset()
+    last_time = time.time()
+    score = 0
+    scores = []
+    episodes = 0
+    last_steps = 1
+
+    while steps < n_steps:
+
+        #action = agent.choose_action(observation)  # this takes and return batches
+        action = env.action_space.sample()
+
+        observation_, reward, done_, trun_, info = env.step(action)
+        done_ = np.logical_or(done_, trun_)
+        steps += 1
+
+        score += reward
+
+        #agent.learn()
+
+        reward = np.clip(reward, -1., 1.)
+
+        #agent.store_transition(observation, action, reward, done_, observation_)
+
+        observation = observation_
+
+        if done_:
+            scores.append(score)
+            score = 0
+            env.reset()
+
+        if steps % 1200 == 0 and len(scores) > 0:
+
+            avg_score = np.mean(scores[-50:])
+
+            if episodes % 1 == 0:
+                print('{} {} avg score {:.2f} total_steps {:.0f} fps {:.2f}'
+                      .format(agent_name, game, avg_score, steps, (steps - last_steps) / (time.time() - last_time)), flush=True)
+                last_steps = steps
+                last_time = time.time()
+
+
