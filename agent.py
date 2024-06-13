75780ecf93591b557d66b3783d227c4a7289bc36
kvarys <k.varys@protonmail.com>
2024-06-13 16:30:13 +0100

    added the noisy nstep 3DQN agent

--- agent.py
+++ agent.py
@@ -2,9 +2,10 @@ import torch as T
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torchsummary import summary
 import numpy as np
 from exploration import EpsilonGreedy
-from networks import DeepQNetwork, DuellingDeepQNetwork
+from networks import DeepQNetwork, DuellingDeepQNetwork, FactorizedNoisyLinear
 from replay_buffer import NStepReplayBuffer, ReplayBuffer
 
 
@@ -120,7 +121,7 @@ class DoubleDQN(DQN):
             return q_target
 
 
-class DoubleDuellingDQN(DoubleDQN):
+class DDDQN(DoubleDQN):
     """Double Duelling DQN"""
 
     def __init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions, max_mem_size=100000, eps_steps=100000,
@@ -128,7 +129,7 @@ class DoubleDuellingDQN(DoubleDQN):
 
         self.name="duellingDoubleDQN"
 
-        DQN.__init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions, max_mem_size, eps_steps, fc1_dims,
+        DoubleDQN.__init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions, max_mem_size, eps_steps, fc1_dims,
                      fc2_dims)
 
     def create_nets(self, fc1_dims, fc2_dims):
@@ -140,11 +141,11 @@ class DoubleDuellingDQN(DoubleDQN):
         return online_net, target_net
 
 
-class NStepDDDQN(DoubleDuellingDQN):
+class NStep3DQN(DDDQN):
     """ double duelling DQN with n-step"""
 
     def __init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions,n=3, fc1_dims=256, fc2_dims=256, max_mem_size=100000, eps_steps=100000):
-        DQN.__init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions, max_mem_size, eps_steps, fc1_dims, fc2_dims)
+        DDDQN.__init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions, max_mem_size, eps_steps, fc1_dims, fc2_dims)
         self.name = "NStep3DQN"
         self.n = n
 
@@ -157,3 +158,23 @@ class NStepDDDQN(DoubleDuellingDQN):
 
             q_target = rewards + self.gamma**self.n * T.max(q_next_target, dim=1)[0]
         return q_target
+
+class NoisyNStep3DQN(NStep3DQN):
+    """ Nstep 3DQN with noisy nets"""
+    def __init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions,n=3, fc1_dims=256, fc2_dims=256, max_mem_size=100000, eps_steps=100000):
+        NStep3DQN.__init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions, n=n, fc1_dims=fc1_dims, fc2_dims=fc2_dims, max_mem_size=max_mem_size, eps_steps=eps_steps)
+
+    def create_nets(self, fc1_dims, fc2_dims):
+        target_net = DuellingDeepQNetwork(self.lr, n_actions=self.n_actions, input_dims=self.input_dims, fc1_dims=fc1_dims, fc2_dims=fc2_dims, device=self.device,linear_layer=FactorizedNoisyLinear)
+        online_net = DuellingDeepQNetwork(self.lr, n_actions=self.n_actions, input_dims=self.input_dims, fc1_dims=fc1_dims, fc2_dims=fc2_dims, device=self.device, linear_layer=FactorizedNoisyLinear)
+        return online_net, target_net
+
+    def choose_action(self, observation):
+        with T.no_grad():
+            self.online_net.reset_noise()
+        return super().choose_action(observation)
+
+    def compute_target(self, rewards, states_, terminals):
+        with T.no_grad():
+            self.target_net.reset_noise()
+        return super().compute_target(rewards,states_,terminals)
